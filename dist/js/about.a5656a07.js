154b6c6a9bb5e7484f86d487e838ca699fd2ba0c
Georgi Naumov <gonaumov@gmail.com>
2021-02-14 23:05:51 +0200

    Deploying

---
+++ dist/js/about.a5656a07.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,c){"use strict";c.r(e);var n=c("7a23");const s={class:"container text-center"},o={class:"row text-center"},r={class:"col-12 text-center"};function a(t,e){return Object(n["s"])(),Object(n["e"])("div",s,[Object(n["h"])("div",o,[Object(n["h"])("div",r,Object(n["B"])(t.$t("aboutDescription")),1)])])}const i={};i.render=a;e["default"]=i}}]);
+//# sourceMappingURL=about.a5656a07.js.map
